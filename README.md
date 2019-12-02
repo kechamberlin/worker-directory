8959adb25e307ed9900223fcc3de44dc029eea72
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-12-02 10:15:02 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# worker-directory
+
+This is a webpage built via react that displays an employee directory (the data of which is stored in a json file).
+
+Each employee card displays a name, occupation, and location.
+
+The user has the ability to deleted an employee card by clicking the red "x" in the top right corner.
---
+++ build/precache-manifest.205ec029d8480ff676b0d2af05e89096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa343289cd803feb35d4d49e1353bc5b",
+    "url": "/kechamberlin/worker-directory/index.html"
+  },
+  {
+    "revision": "aed1ba593f842cf5f9fa",
+    "url": "/kechamberlin/worker-directory/static/css/main.d9ef5748.chunk.css"
+  },
+  {
+    "revision": "090719cf62ab679aeb51",
+    "url": "/kechamberlin/worker-directory/static/js/2.1f87a36b.chunk.js"
+  },
+  {
+    "revision": "aed1ba593f842cf5f9fa",
+    "url": "/kechamberlin/worker-directory/static/js/main.1aa9f8fd.chunk.js"
+  },
+  {
+    "revision": "4c7ea75725a0a3368bd0",
+    "url": "/kechamberlin/worker-directory/static/js/runtime-main.872ee8f7.js"
+  }
+]);
